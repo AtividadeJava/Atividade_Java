4e8a61c0bdc3944f3c951d883e372a98b717c582
TH13L26 <95258968+TH13L26@users.noreply.github.com>
2023-11-17 20:46:18 -0300

    Add files via upload

---
+++ AtividadeJava/src/AtividadeJava/Exercicio36.java
@@ -0,0 +1,11 @@
+package Exercicios110;
+
+public class Exercicio36 {
+    public static void main (String[] args){
+        int contar = 1;
+        while (contar <= 10) {
+            System.out.println(contar);
+            contar++;
+        }
+    }
+}
